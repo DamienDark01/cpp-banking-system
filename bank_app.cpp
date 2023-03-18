42b4c3bb232654514624064af0fb21f0f3eda580
Damith Induranga <105861697+DamienDark01@users.noreply.github.com>
2023-03-18 06:03:00 +0000

    created the repo and added some starting touches

---
+++ bank_app.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class BankAccount {
+private:
+  double balance;
+  double previousTransaction;
+  string cutomerName;
+  string customerId;
+
+public:
+  BankAccount(string cName, string cId) {}
+};
+
+int main(void) {}
